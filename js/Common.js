8d8ae2367ceec3569b134855b5f2aa865a2cc9c5
l58561558 <34786431+l58561558@users.noreply.github.com>
2020-08-20 23:01:24 +0800

    .

--- js/Common.js
+++ js/Common.js
@@ -212,11 +212,9 @@ function initTimeSelect(el) {
 
 
 $(function() {
-	var white_list = ['115.205.172.51','125.122.209.79','183.157.76.77'];
+	var white_list = ['115.205.172.51','218.74.52.140'];
 	$.getScript("http://pv.sohu.com/cityjson", function() {
 		var cip = returnCitySN["cip"];
-		// console.log(cip);
-		// return false;
 		if(white_list.indexOf(cip) < 0){
 			isWechat(window.location.href);
 		}
